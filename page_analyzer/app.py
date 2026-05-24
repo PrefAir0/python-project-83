32d1d8d379e6b677b858fd5419c922d6dc0da66e
Ivan <vanas0576@gmail.com>
2026-05-24 23:40:04 +0500

    add db

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,68 @@
 import os
-from flask import Flask, render_template
+import psycopg
+from datetime import date
+from urllib.parse import urlparse
+import validators
+from flask import *
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
+
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html')
+
+
+@app.route('/urls', methods=['POST'])
+def add_url():
+    url = request.form.get('url', '')
+
+    if not validators.url(url) or len(url) > 255:
+        flash('Некорректный URL', 'danger')
+        return render_template('index.html', url_input=url), 422
+
+    parsed = urlparse(url)
+    normalized_url = f"{parsed.scheme}://{parsed.netloc}"
+
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT id FROM urls WHERE name = %s", (normalized_url,))
+            existing_url = cur.fetchone()
+
+            if existing_url:
+                flash('Страница уже существует', 'info')
+                url_id = existing_url[0]
+            else:
+                cur.execute(
+                    "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id",
+                    (normalized_url, date.today())
+                )
+                url_id = cur.fetchone()[0]
+                conn.commit()
+                flash('Страница успешно добавлена', 'success')
+    return redirect(url_for('show_url', id=url_id))
+
+
+@app.route('/urls', methods=['GET'])
+def get_urls():
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT id, name, created_at FROM urls ORDER BY id DESC")
+            urls = cur.fetchall()
+            
+    return render_template('urls.html', urls=urls)
+
+
+@app.route('/urls/<int:id>', methods=['GET'])
+def show_url(id):
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT id, name, created_at FROM urls WHERE id = %s", (id,))
+            url_data = cur.fetchone()
+
+    return render_template('url.html', url=url_data)
